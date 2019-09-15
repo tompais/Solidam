f5dbebb61eecde8f43733b8e8a98c2d79abfca41
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-15 15:33:42 -0300

    Mejoras y reacomodamiento del proyecto

---
+++ src/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+
+        public BaseModel()
+        {
+        }
+
+        public BaseModel(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Solidam/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Mvc;
+
+namespace Solidam.Controllers
+{
+    public class BaseController : Controller
+    {
+    }
+}
