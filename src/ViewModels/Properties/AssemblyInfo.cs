9e2ffd4f47a3fa7b9399bfc85a54bb23c7bf20a3
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-17 21:04:50 -0300

    Agrego más librerías de clase a la solución y limpio código

--- src/ViewModels/Properties/AssemblyInfo.cs
+++ src/ViewModels/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solidam")]
+[assembly: AssemblyTitle("ViewModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solidam")]
+[assembly: AssemblyProduct("ViewModels")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3067b3bf-1056-41d9-a48c-35a474e7105a")]
+[assembly: Guid("bfb013a9-d0d5-4c03-a2a8-6d9cbd88d693")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +28,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
