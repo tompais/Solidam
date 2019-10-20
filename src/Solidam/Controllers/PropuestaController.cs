76fbaef13e4d8ad261b7beed621d21b835784a4b
ezequiel.allio <ezequiel.allio@gmail.com>
2019-10-20 19:18:36 -0300

    controlador propuesta y mini avance

---
+++ src/Solidam/Controllers/PropuestaController.cs
@@ -0,0 +1,23 @@
+﻿using Helpers;
+using Models;
+using Services;
+using System.Linq;
+using System.Web.Mvc;
+using Utils;
+
+namespace Solidam.Controllers
+{
+    public class PropuestaController : BaseController
+    {
+        public ActionResult CrearPropuesta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Propuesta p)
+        {
+            return View();
+        }
+    }
+}
