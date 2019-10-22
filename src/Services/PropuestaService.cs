1e362ea1a8ec0894986f03d322c43705023043bb
ezequiel.allio <ezequiel.allio@gmail.com>
2019-10-22 11:56:52 -0300

    avances en crear propuesta

---
+++ src/Services/PropuestaService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Services
+{
+    public class PropuestaService : BaseService<PropuestaService>
+    {
+        public static void AgregarPropuesta(Propuesta p)
+        {
+            p.IdUsuarioCreador = Helpers.SessionHelper.Usuario.IdUsuario;
+            p.Estado = 0;
+            p.FechaCreacion = System.DateTime.Today;
+
+            
+
+            Db.Propuesta.Add(p);
+            Db.SaveChanges();
+        }
+
+        public static int TotalPropuestasActivas()
+        {
+            return Db.Propuesta.Count(x => x.Estado == 0 && x.IdUsuarioCreador == Helpers.SessionHelper.Usuario.IdUsuario);
+        }
+    }
+}
