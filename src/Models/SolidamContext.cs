bbb603b5c11cfa51a9cba2834abe3f962e3be67c
Tomás Pais <tomas.j.pais@gmail.com>
2019-10-26 20:00:58 -0300

    Uso patrón singleton de doble chequeo para solidam context

--- src/Models/SolidamContext.cs
+++ src/Models/SolidamContext.cs
@@ -6,6 +6,22 @@
         {
         }
 
-        public static SolidamContext Instance { get; } = new SolidamContext();
+        private static SolidamContext _instance;
+        private static readonly object Obj = new object();
+
+        public static SolidamContext Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (Obj)
+                {
+                    if (_instance == null)
+                        _instance = new SolidamContext();
+                }
+
+                return _instance;
+            }
+        }
     }
 }
