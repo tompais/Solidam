0755e4267bc59c3ee81205e7fba2c7004a7a14ed
Ignacio Crescenzo <ignadario97@gmail.com>
2019-10-27 01:41:22 -0300

    Mejoras validacion js

--- src/Solidam/Scripts/Views/Propuesta/Donar.js
+++ src/Solidam/Scripts/Views/Propuesta/Donar.js
@@ -3,6 +3,7 @@
         $(this).parent().parent().find("input[type=number]").removeClass("d-none");
     } else {
         $(this).parent().parent().find("input[type=number]").addClass("d-none");
+        $(this).parent().parent().find(".error").addClass("d-none");
     }
 });
 
@@ -11,13 +12,12 @@ $("#btnConfirmar").click(function () {
     var validacion = true;
     $(".check:checked").each(function (index, value) {
         if (!validarNumero(parseInt($(value).parent().parent().find("input[type=number]").val()), $(value))) validacion = false;
-        validacion = true;
         var donacionInsumo = {};
         donacionInsumo.idPropuestaDonacionInsumo = $(value).attr("don-id");
         donacionInsumo.cantidad = $(value).parent().parent().find("input[type=number]").val();
         donaciones.push(donacionInsumo);
     });
-    if (validacion) {
+    if (validacion && $(".check:checked").length > 0) {
         $.ajax({
             url: window.pathCrearDonacionInsumo,
             data: { donaciones: donaciones },
@@ -31,8 +31,8 @@ $("#btnConfirmar").click(function () {
 
 function validarNumero(cantidad,element) {
     var restante = element.parent().parent().find(".rest").text();
-    if (cantidad > restante || cantidad <= 0) {
-        element.parent().parent().find(".error").text("No se puede ingresar esa cantidad");
+    if (cantidad > restante || cantidad <= 0 || isNaN(cantidad)) {
+        element.parent().parent().find(".error").text(isNaN(cantidad) ? "Debe ingresar un valor" : "No se puede ingresar esa cantidad");
         element.parent().parent().find(".error").removeClass("d-none");
         return false;
     } else {
