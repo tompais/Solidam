20e8a82ad390440711cd94851e78fae882a20e8b
Tomás Pais <tomas.j.pais@gmail.com>
2019-10-26 22:49:58 -0300

    Configo controller de usuario para enrutamiento al perfil

--- src/Solidam/Controllers/UsuarioController.cs
+++ src/Solidam/Controllers/UsuarioController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Helpers;
+using Models;
 
 namespace Solidam.Controllers
 {
     public class UsuarioController : BaseController
     {
-        [Route("perfil")]
+        // GET: Perfil
+        [Route("miPerfil")]
         public ActionResult MiPerfil()
         {
-            return View();
+            return View(new UsuarioPerfil(SessionHelper.Usuario));
+        }
+
+        [HttpPost]
+        public ActionResult GuardarPerfil()
+        {
+            return null;
         }
     }
 }
