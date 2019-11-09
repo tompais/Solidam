20d6ddc7d52bc06a4e2db911ea57d4e0f9e6f345
Ignacio Crescenzo <ignadario97@gmail.com>
2019-11-09 12:13:26 -0300

    Api controller funcando

---
+++ src/Solidam/Controllers/DonacionesApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using System.Web.Routing;
+namespace Solidam.Controllers
+{
+    public class DonacionesApiController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        public JsonResult<string> Get()
+        {
+            return Json("Hola");
+        }
+    }
+}
--- src/Solidam/Global.asax.cs
+++ src/Solidam/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using Helpers;
 using System;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -23,6 +24,7 @@ namespace Solidam
 
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
