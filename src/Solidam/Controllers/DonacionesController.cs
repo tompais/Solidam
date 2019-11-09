88167e8ca9202f5c60ff10e7247ed7357aa26490
Tomás Pais <tomas.j.pais@gmail.com>
2019-11-09 19:41:03 -0300

    Creo controller de donaciones

---
+++ src/Solidam/Controllers/DonacionesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solidam.Controllers
+{
+    public class DonacionesController : Controller
+    {
+        public ActionResult Historial()
+        {
+            return View();
+        }
+    }
+}
