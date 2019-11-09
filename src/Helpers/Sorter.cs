f6fa475f6ce624cdef45f7edf36f5327069688f6
Tomás Pais <tomas.j.pais@gmail.com>
2019-11-09 19:38:47 -0300

    Agrego Sorter para usar de Helper

---
+++ src/Helpers/Sorter.cs
@@ -0,0 +1,14 @@
+﻿namespace Helpers
+{
+    public class Sorter
+    {
+        public string Property { get; set; }
+        public string Direction { get; set; }
+
+        public Sorter(string property, string direction)
+        {
+            Property = property;
+            Direction = direction;
+        }
+    }
+}
