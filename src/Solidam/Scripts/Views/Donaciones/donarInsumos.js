f8aca68d26e3fd0cff3e7a5fa3466362209c335d
Ignacio Crescenzo <ignadario97@gmail.com>
2019-11-12 17:15:57 -0300

    Validacion que no deje entrar in items checkeados

---
+++ src/Solidam/Scripts/Views/Donaciones/donarInsumos.js
@@ -0,0 +1,15 @@
+﻿$("#btnContinuarInsumos").click(function (event) {
+    event.preventDefault();
+    var validation = false;
+    $.each($(".check"),
+        function(index, value) {
+            if ($(value).is(":checked")) {
+                validation = true;
+            }
+        });
+    if (validation) {
+        $(".error").addClass("d-none");
+        $("form").submit();
+    } else
+        $(".error").removeClass("d-none");
+});
